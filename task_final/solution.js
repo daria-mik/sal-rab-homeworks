56cdb4fbc223a0701c1c23c96802197adb0bb2f4
daria-mik <90906407+daria-mik@users.noreply.github.com>
2021-10-11 16:38:49 +0300

    Add files via upload

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,8 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: {name, phone},order: {address, sum}, goods: [{title, count}]};
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
@@ -47,7 +46,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
