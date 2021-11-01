8666e20cd459629f65927c0c2728927cdacc9cb7
daria-mik <90906407+daria-mik@users.noreply.github.com>
2021-11-01 22:19:59 +0300

    Add files via upload

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,20 +33,23 @@
 //    }
 // }
 
+
+
 function sendRequest(name, phone, address, goods, sum) {
-    let goods = {client: {name, phone},order: {address, sum}, goods: [{title, count}]};
-    
+    let data = {order: {},goods: [] };
+
     let countOfGoods = goods.length;
-    
-        for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
 
+    for (let i = 0; i < countOfGoods; i +=1) {
+        data.goods.push ({title: goods[i].title, count: goods[i].count});
     }
-    data.order.address = "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53";
-    data.order.sum = sum;
-    data.client.name = 'Иван'+ '+7(987)65-43-210';
 
-    let jsonData = JSON.stringify({data})
+    data.order.address = "ул." + " " + address.street + ", дом" + " " + address.house + "," + " " + address.entrance + " подъезд," + " " + address.floor + " " + "этаж," + " " + "кв" + " " + address.flat;
+    data.order.sum = sum;
+
+    data.client = name + " " + phone;
+
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
